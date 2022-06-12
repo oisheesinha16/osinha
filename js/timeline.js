31942f8aec3004de918dc54d1eaf39409264f71a
nihalcshah <cjshah00@gmail.com>
2022-06-12 02:41:03 -0400

    Slide ins

---
+++ js/timeline.js
@@ -0,0 +1,35 @@
+function revealleft() {
+    var reveals = document.querySelectorAll(".reveal-left");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 0;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+function revealright() {
+    var reveals = document.querySelectorAll(".reveal-right");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 0;
+
+        if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+        } else {
+        reveals[i].classList.remove("active");
+        }
+    }
+}
+  
+window.addEventListener("scroll", revealright);
+window.addEventListener("scroll", revealleft);
+  
